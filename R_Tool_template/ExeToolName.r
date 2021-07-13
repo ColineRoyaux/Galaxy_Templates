e0b3126f2de887c0b5ae053128ff28eb43dda6c6
ColineRoyaux <61148928+ColineRoyaux@users.noreply.github.com>
2021-07-13 17:14:15 +0200

    Update ExeToolName.r

--- R_Tool_template/ExeToolName.r
+++ R_Tool_template/ExeToolName.r
@@ -19,16 +19,19 @@ if (length(args)==0)
     sep <- as.character(args[2])
     dec <- as.character(args[3])
     HR <- args[4]
+    #Add any other argument you need
 }
 
+#Correcting the Galaxy boolean to the R boolean
 if (HR == "false"){ HR <- FALSE } else { HR <- TRUE }
 
 #####Import data
 Data <- read.table(Table,sep=sep,dec=dec,header=HR,encoding="UTF-8")
 
 
-#####Your analysis
+#####Your analysis : write your generalized executive steps 
 
-sum <- capture.output(summary(Data))
 
-write(sum, "Summary.txt")
+
+#####Write output in a file
+write(sum, "filename.txt")
